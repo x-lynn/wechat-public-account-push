183cf51557b4a4b62d57488d5878ce1ad71bf625
x-lynn <112162944+x-lynn@users.noreply.github.com>
2022-12-22 06:07:21 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,9 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'server-chan',
+  
   TIAN_API:{key:'74172b4477599d545841fd0b7c96d0bc'},
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx4d9e3084bc34c6a9',
