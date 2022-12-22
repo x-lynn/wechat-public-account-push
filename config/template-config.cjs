484f494c9cbf4c1412f002cd561b36114611f91a
x-lynn <112162944+x-lynn@users.noreply.github.com>
2022-12-22 07:39:34 +0000

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -34,6 +34,40 @@ const TEMPLATE_CONFIG = [
       
       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
+  },{
+    id: '00010',
+    title: '亲爱的刘小仙女, 早上好',
+    desc: `
+      **{{date.DATA}}**
+      
+      下个休息日：{{holidaytts.DATA}}
+      
+      ---
+      
+      城市：{{city.DATA}}
+      
+      天气：{{weather.DATA}}
+      
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
+      风向: {{wind_direction.DATA}}
+      
+      风级: {{wind_scale.DATA}}
+      
+      {{comprehensive_horoscope.DATA}}
+      
+      ---
+      
+      今天是我们相识的第{{love_day.DATA}}天
+      
+      {{birthday_message.DATA}}
+      
+      ---
+      
+      {{moment_copyrighting.DATA}}
+      
+      {{poetry_title.DATA}} {{poetry_content.DATA}}
+    `
   },
   {
     id: '0002',
