1a1df8a16e9fb2e43a3590b0ccf6f5d21235cfdf
x-lynn <112162944+x-lynn@users.noreply.github.com>
2022-12-22 07:18:49 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,35 +22,6 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '自己',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT150188TiW44CLs7Yvtc3Oy5Moz2Vi3p',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '01-25',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '自己', year: '1997', date: '12-27',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '自己', year: '1998', date: '01-25',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2022', date: '11-22',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-11-22' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-11-22' },
-      ],
-    },{
-      // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyk9g6elGL95ZtjCE8YIsfPq9c3w',
@@ -66,6 +37,35 @@ const USER_CONFIG = {
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '宝贝', year: '2001', date: '11-17',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '11-22',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-11-22' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-11-22' },
+      ],
+    },{
+      // 想要发送的人的名字
+      name: '自己',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'SCT150188TiW44CLs7Yvtc3Oy5Moz2Vi3p',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0001',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '01-25',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '自己', year: '1997', date: '12-27',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '自己', year: '1998', date: '01-25',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '11-22',
