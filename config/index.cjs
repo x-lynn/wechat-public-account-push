409f3b80374a3ba02fa888114b72307082eea511
x-lynn <112162944+x-lynn@users.noreply.github.com>
2022-12-22 06:23:00 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyk9g6YYuJmKeJz_esZCPyIj4oM4',
+      id: 'SCT150188TiW44CLs7Yvtc3Oy5Moz2Vi3p',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'e5rdoHCvQtBcl_6iRENBP_7MaZ3HSPtz3Wk6j8zZXlk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
