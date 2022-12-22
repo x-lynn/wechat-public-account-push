234bbea195f367840ebfe99c5576e5124fe8d43a
x-lynn <112162944+x-lynn@users.noreply.github.com>
2022-12-22 07:40:26 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'SCT189031Tu4JcwebCZiTWV9F4CcjeREjD',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '00010',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-17',
       festivals: [
