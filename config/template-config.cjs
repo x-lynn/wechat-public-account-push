54c7e0f6a6baf39204bd07517c7eb3bf9f1a2a0a
Christian Wilkinson <112162944+x-lynn@users.noreply.github.com>
2023-07-13 01:18:54 +0000

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -25,6 +25,7 @@ const TEMPLATE_CONFIG = [
       ---
       
       今天是我们相识的第{{love_day.DATA}}天
+      今天是我们结婚的第{{marry_day.DATA}}天
       
       {{birthday_message.DATA}}
       
@@ -59,6 +60,7 @@ const TEMPLATE_CONFIG = [
       ---
       
       今天是我们相识的第{{love_day.DATA}}天
+      今天是我们结婚的第{{marry_day.DATA}}天
       
       {{birthday_message.DATA}}
       
