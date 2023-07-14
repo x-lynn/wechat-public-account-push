6366155c6851ec72fb6ef54299ad1f7c98ee586d
Christian Wilkinson <112162944+x-lynn@users.noreply.github.com>
2023-07-14 02:01:58 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '玲宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'SCT189031Tu4JcwebCZiTWV9F4CcjeREjD',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -32,11 +32,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝-农历', year: '2001', date: '10-03',
+          type: '*生日', name: '玲宝宝-农历', year: '2001', date: '10-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝贝-公历', year: '2001', date: '11-17',
+          type: '生日', name: '玲宝宝-公历', year: '2001', date: '11-17',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '11-22',
