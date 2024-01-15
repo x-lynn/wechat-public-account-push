a4d6c3e4249c164614eb37b3b1c364bd739c67b8
Coral Strosin <112162944+x-lynn@users.noreply.github.com>
2024-01-15 09:23:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,8 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '玲宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT189031Tu4JcwebCZiTWV9F4CcjeREjD',
+      //id: 'SCT189031Tu4JcwebCZiTWV9F4CcjeREjD',
+      id: 'oyk9g6elGL95ZtjCE8YIsfPq9c3w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '00010',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -56,7 +57,8 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝-自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT150188TiW44CLs7Yvtc3Oy5Moz2Vi3p',
+      //id: 'SCT150188TiW44CLs7Yvtc3Oy5Moz2Vi3p',
+      id: 'oyk9g6YYuJmKeJz_esZCPyIj4oM4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '00010',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
