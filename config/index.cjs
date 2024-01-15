e38f2b607190e4de57b4d8794539cdc3c54ebca4
Coral Strosin <112162944+x-lynn@users.noreply.github.com>
2024-01-15 10:23:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  //USE_PASSAGE: 'server-chan',
+  USE_PASSAGE: 'server-chan',
   TIAN_API:{key:'74172b4477599d545841fd0b7c96d0bc'},
   APP_ID: 'wx4d9e3084bc34c6a9',
   APP_SECRET: '76aa1a3cc6cefbf2726109935073e32c',
@@ -17,16 +17,20 @@ const USER_CONFIG = {
   USERS: [
     {
       name: '玲宝宝',
-      id: 'oyk9g6elGL95ZtjCE8YIsfPq9c3w',
-      useTemplateId: 'cmlxbNiGRZ9uKQkdEELEJ3YqpCgmnsX0Gk_5jTaGBNg',
+      //id: 'oyk9g6elGL95ZtjCE8YIsfPq9c3w',
+      id: 'SCT189031Tu4JcwebCZiTWV9F4CcjeREjD',
+      useTemplateId: '00011',
+      //useTemplateId: 'cmlxbNiGRZ9uKQkdEELEJ3YqpCgmnsX0Gk_5jTaGBNg',
     },
   ],
 
-  CALLBACK_TEMPLATE_ID: 'q9CS-cnCdZ14yz1am5IpEQpB7Q_DaCdfpU5wDXgFq1I',
+  CALLBACK_TEMPLATE_ID: '0002',
+  //CALLBACK_TEMPLATE_ID: 'q9CS-cnCdZ14yz1am5IpEQpB7Q_DaCdfpU5wDXgFq1I',
   CALLBACK_USERS: [
     {
       name: '自己',
-      id: 'oyk9g6YYuJmKeJz_esZCPyIj4oM4',
+      id: 'SCT150188TiW44CLs7Yvtc3Oy5Moz2Vi3p',
+      //id: 'oyk9g6YYuJmKeJz_esZCPyIj4oM4',
     }
   ],
 
